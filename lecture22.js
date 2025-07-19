e73606f6a54f5a3351a7d2769caa98aa6ba81287
Chetan Sarswat <sarswatchetan97@gmail.com>
2025-07-19 21:58:36 +0530

    Updated with example

--- lecture22.js
+++ lecture22.js
@@ -1,11 +1,27 @@
 // async makes a function return Promise.
 // await pauses the function execution until the Promise is resolved or rejected.
 
-async function myFunction() {
-    try{
-        const res = await someAsyncTask()
-        console.log(res);
-    } catch (error) {
-        console.log(error);
+// async function myFunction() {
+//     try{
+//         const res = await someAsyncTask()
+//         console.log(res);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+//Scenario
+
+async function fetchUserData() {
+    console.log('Fetching Data...');
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+        const data = await response.json();
+        console.log('Name:', data.name, ' ','Username:', data.username);
+    } catch(error) {
+        console.log('Error fetching data', error);
+    } finally {
+        console.log('Thank you!!');
     }
 }
+fetchUserData();
