aad6d9113f8e2ba7058b8a241a4f2c6f263f33cb
Chetan Sarswat <sarswatchetan97@gmail.com>
2025-07-25 09:52:30 +0530

    ES6 module

---
+++ index.js
@@ -0,0 +1,15 @@
+// var val1 = 10;
+// var val2 = 25;
+
+// function addNum(num1, num2) {
+//     return num1 + num2;
+// }
+
+// var result = addNum(val1, val2);
+
+// console.log(result);
+
+import { add, multiply } from './lecture24.js'
+
+console.log(add(2,3));
+console.log(multiply(5,6));
---
+++ lecture24.js
@@ -0,0 +1,4 @@
+//ES6 modules in JS
+
+export const add = (a,b) => a+b;
+export const multiply = (a,b) => a*b;
