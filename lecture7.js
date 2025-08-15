b38311bbddbf81a381cc356c1e049056788be7ec
Chetan Sarswat <sarswatchetan97@gmail.com>
2025-08-15 10:55:18 +0530

    Private access specified in JS

---
+++ lecture7.js
@@ -0,0 +1,26 @@
+class Person {
+    #name; //defined Private Variable
+    constructor(name) {
+        this.#name = name;
+    }
+    get name() {
+        return this.#name;
+    }
+    set setName(newName) {
+        if(newName) {
+            this.#name = newName;
+        } else {
+            console.log("New name cannot be empty");
+        }
+    }
+}
+
+//console.log(#name); -> cannot be accessed outside class - gives SyntaxError.
+
+const person = new Person('Chetan');
+console.log(person.name);
+
+person.setName = '';
+console.log(person.name);
+
+
